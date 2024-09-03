dc31c83aae436372ceb5400ea6d4f0550db6b2ee
zeethefedev <zeethefrontenddev@gmail.com>
2024-09-03 20:52:32 +0700

    add confirm popup

--- src/components/ConfirmPopup.vue
+++ src/components/ConfirmPopup.vue
@@ -1,9 +1,62 @@
 <template>
-  <div></div>
+  <div v-if="open" class="overlay-wrapper">
+    <div class="overlay max-h-[80vh] overflow-scroll hide-scrollbar">
+      <button class="w-auto absolute top-11 right-11" @click="$emit('click')">
+        <SVGIcon icon="close" viewBox="0 0 512 512" />
+      </button>
+      <div class="overlay-content"><slot></slot></div>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {}
+import SVGIcon from './SVGIcon.vue'
+
+export default {
+  components: { SVGIcon },
+  props: {
+    open: {
+      type: Boolean,
+      default: false
+    }
+  }
+}
 </script>
 
-<style scoped></style>
+<style scoped>
+.overlay-wrapper {
+  width: 100%;
+  height: 100%;
+  position: fixed;
+  background: rgba(0, 0, 0, 0.5);
+  z-index: 10;
+  top: 0;
+  left: 0;
+  /* center dialog overlay */
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  gap: 16px;
+}
+
+.overlay {
+  width: 50%;
+  /* overflow: scroll; */
+  background: var(--color-background);
+  border-radius: 4px;
+  border: 2px black solid;
+  padding: 44px;
+  transform: translate(-2px, -2px);
+  border: 2px solid var(--color-border);
+  color: var(--color-text);
+}
+
+.overlay-content {
+  position: relative;
+}
+
+.hide-scrollbar::-webkit-scrollbar {
+  display: none;
+}
+</style>
--- src/views/MyArtworkView.vue
+++ src/views/MyArtworkView.vue
@@ -1,24 +1,51 @@
 <template>
   <art-list :artworks="initialArtworks" />
-  <button @click="handleSave">Save</button>
+  <button @click="handleOpen">Save</button>
+  <confirm-popup :open="open" @click="handleClose">
+    <h2 class="text-xl mb-4">Do you want to save your new artworks?</h2>
+    <div class="flex gap-2 w-full justify-end">
+      <button @click="handleClose">No</button>
+      <button @click="handleSave">Yes</button>
+    </div>
+  </confirm-popup>
 </template>
 
 <script>
 import ArtList from '@/components/ArtList.vue'
-import { computed } from 'vue'
+import { computed, ref, watch } from 'vue'
 import { useStore } from 'vuex'
+import ConfirmPopup from '@/components/ConfirmPopup.vue'
 export default {
-  components: { ArtList },
+  components: { ArtList, ConfirmPopup },
   setup() {
     const store = useStore()
     const artworks = computed(() => store.state.likedArtworks)
     const initialArtworks = computed(() => store.state.initialLikedArtworks)
+    const body = document.getElementsByTagName('body')
+
+    const open = ref(false)
+
+    const handleOpen = () => {
+      open.value = true
+    }
+
+    const handleClose = () => {
+      open.value = false
+    }
 
     const handleSave = () => {
       store.commit('saveLike', artworks)
+      handleClose()
     }
 
-    return { artworks, initialArtworks, handleSave }
+    watch(open, (open) => {
+      if (body) {
+        if (open) body[0].style.overflow = 'hidden'
+        else body[0].style.overflow = 'visible'
+      }
+    })
+
+    return { artworks, initialArtworks, handleSave, open, handleOpen, handleClose }
   }
 }
 </script>
